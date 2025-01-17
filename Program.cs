d18d8e4d269b35c1d7107df8642172ca8e6baf13
Miraj-cybe <mondolm619@gmail.com>
2025-01-18 01:00:23 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Assignment_1{
+    public static void Main(string[] args)
+    {
+      string myNAme="Miraj Modol";
+        Console.WriteLine(myNAme);
+         myNAme="sajida";
+         Console.WriteLine(myNAme);
+
+int Num1;
+int Num2;
+int Num3;
+// we can also write: int Num1,Num2,num3
+
+Num1=10;
+Num2=10;
+Num3=10;
+//we can write like: Num1=Num2=num3
+Console.WriteLine(Num2);
+    }
+   
+}
